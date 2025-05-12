baf88020d937ffddbd897dbfbaedd8e8e449b68c
effiong20 <effiongeno20@yahoo.com>
2025-05-12 13:51:28 +0100

    done fix

--- jenkins-server-terraform/scripts/tools-install.sh
+++ jenkins-server-terraform/scripts/tools-install.sh
@@ -6,7 +6,7 @@ sudo apt install openjdk-17-jre -y
 sudo apt install openjdk-17-jdk -y
 java --version
 
-# Installing Jenkins
+### Installing Jenkins
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
